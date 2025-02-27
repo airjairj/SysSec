e8e6f06fc80c4e99905b11fb807443750d581fa9
FranAva001 <115705321+FranAva001@users.noreply.github.com>
2025-02-27 11:39:14 +0100

    Update app.py

--- HW345/webapp/app.py
+++ HW345/webapp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, flash, render_template, request, redirect, session,  url_for
 import requests
+import jwt
+from urllib.parse import urlencode
 import secrets
 import vakt
 from vakt.rules import Eq, StartsWith, And, Greater, Less, Any # Importa tutto il set di regole di VAKT
@@ -7,8 +9,8 @@ import urllib3
 from datetime import timedelta, datetime
 from flask_sqlalchemy import SQLAlchemy
 from authlib.integrations.flask_client import OAuth
-from authlib.jose import jwt
 
+import logging
 urllib3.disable_warnings() # Serve perchè senza non funziona bene la webapp dato che i certificati sono auto firmati
 
 app = Flask(__name__)
@@ -17,15 +19,13 @@ app.permanent_session_lifetime = timedelta(minutes=10)  # AC-12: Inattività mas
 oauth = OAuth(app)
 VAULT_ADDR = "https://localhost:8200"  # Usa HTTP se Vault è configurato senza HTTPS
 VAULT_VERIFY = False  # Imposta su True se Vault è configurato con un certificato SSL valido
+redirect_uri = "https://localhost:5173"
+keycloak_url = "http://localhost:8080"
+realm_name = "SystemSecurity"
+client_id = "FranAva"
+client_secret = "Y00EvlImnhpuC5XOpHLxC2sly5BmQT4u"
+token_url = "https://oauth2.googleapis.com/token"
 
-oauth.register(
-    name="google",
-    client_id="292219212098-q10f6k4mgjkp8lj7v0drpu0hqdpsrqfr.apps.googleusercontent.com",
-    client_secret="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",
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    redirect_uri='https://localhost:5173/callback',
-    client_kwargs={"scope": "openid email profile"},
-)
 
 # VAKT
 # Definisci le policy in VAKT
@@ -85,12 +85,12 @@ storage = vakt.MemoryStorage()
 for policy in policies:
     storage.add(policy)
 guard = vakt.Guard(storage, vakt.RulesChecker())
-app = Flask(__name__)
 
 
 # DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/francescoavallone/Desktop/UNIVERSITA/SystemSecurity/SysSec/HW345/webapp/data/notes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/francescoavallone/Desktop/SysSec/HW345/webapp/data/notes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 db = SQLAlchemy(app)
 
 # Struttura della tabella delle note
@@ -179,37 +179,98 @@ def login():
     
 @app.route('/loginOPENID')
 def loginOPENID():
-    nonce = secrets.token_urlsafe(16)
-    session["oauth_state"] = {"nonce": nonce}
-    return oauth.google.authorize_redirect(url_for("callback", _external=True), nonce=nonce)
+
+    authorize_url = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/auth"
+    redirect_uri_callback = f"{redirect_uri}/callback"
+    params = { 'client_id': client_id, 'redirect_uri': redirect_uri_callback, 'response_type': 'code', 'scope': 'openid profile email' }
+
+
+    if 'username' not in session: 
+        return redirect(f"{authorize_url}?{'&'.join([f'{key}={value}' for key, value in params.items()])}")
+
+
+    userinfo_endpoint = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/userinfo"
+    headers = {'Authorization': f"Bearer {session['access_token']}"}
+    response = requests.get(userinfo_endpoint, headers=headers)
+
+    if response.status_code == 200:
+        return redirect("/dashboard")
+    else:
+        logging.info("Access token scaduto, tentativo di refresh...")
+        refresh_token = session['user']['refresh_token']
+        token_endpoint = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/token"
+        payload = { 'client_id': client_id, 'grant_type': 'refresh_token', 'refresh_token': refresh_token, 'client_secret': client_secret}
+        token_response = requests.post(token_endpoint, data=payload)
+        if token_response.status_code == 200: # ma il refresh token è ancora valido
+            new_tokens = token_response.json()
+            session['access_token'] = new_tokens['access_token']
+            session['refresh_token'] = new_tokens['refresh_token']
+            session['id_token'] = new_tokens['id_token']
+            logging.info("Token aggiornati con successo.")
 
 @app.route("/callback")
 def callback():
-    token = oauth.google.authorize_access_token()
-    nonce = session.get("oauth_state", {}).get("nonce")
-    user_info = oauth.google.parse_id_token(token, nonce=nonce)
+    code = request.args.get('code')
+    logging.debug(f"Callback received with code: {code}")
+    redirect_uri_callback = f"{redirect_uri}/callback"
 
-    session['user'] = user_info
-    session["username"] = user_info["name"] + " (" + user_info["email"] +")"
-    session["role"] = "utente"
+    token_endpoint = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/token"
 
-    url = f"{VAULT_ADDR}/v1/auth/jwt/login"
-    headers = {"Content-Type": "application/json"}
-    payload = {"role": "utente", "jwt": token["id_token"]}
+    payload = { 
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirect_uri_callback,
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
 
-    response = requests.post(url, json=payload, headers=headers, verify=False)
+    try:
+        response = requests.post(token_endpoint, data=payload)
+        logging.debug(f"payload: {response}")
+        token_data = response.json()
+        print(token_data)
 
-    data = response.json()
+        if 'access_token' in token_data:
+            userinfo_endpoint = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/userinfo"
+            headers = {'Authorization': f"Bearer {token_data['access_token']}"}
+            userinfo_response = requests.get(userinfo_endpoint, headers=headers)
+            userinfo = userinfo_response.json()
+            
+            session['access_token'] = token_data.get('access_token')
+            session['refresh_token'] = token_data.get('refresh_token')
+            session['username'] = userinfo.get('preferred_username')
+            session['email'] = userinfo.get('email')
+            session['role'] = "amministratore"
+            session["id_token"] = token_data.get('id_token')
 
-    session["vault_token"] = data["auth"]["client_token"]
-    
-    return redirect("/dashboard")
+            decoded_token = jwt.decode(session['access_token'], options={"verify_signature": False})
+            roles = decoded_token.get('realm_access', {}).get('roles', [])
+            if "Amministratore" in roles: 
+                session["role"] = "amministratore"
+            elif "Utente" in roles: 
+                session["role"] = "utente"
+            else: 
+                return "Denied Access."
+
+
+            logging.debug("User logged in successfully.")
+            return redirect("/dashboard")
+        else:
+            logging.error("Failed to fetch tokens.")
+            return "Failed to fetch tokens."
+
+    except Exception as e:
+        logging.error(f"Exception during token exchange: {e}")
+        return "Failed to fetch tokens."
+
 
 @app.route("/dashboard", methods=["GET", "POST"])
 @app.route("/tabella", methods=["GET", "POST"])
 def notes():
-    if "vault_token" not in session:
-        return redirect("/")
+    referrer = request.referrer
+    if referrer and "login" in referrer:
+        if "vault_token" not in session:
+            return redirect("/")
 
     username = session["username"]
     role = session.get("role", "utente")
@@ -223,91 +284,145 @@ def notes():
 
 @app.route("/add-note", methods=["GET", "POST"])
 def add_note():
-    if "vault_token" not in session:
-        return redirect("/")
+    referrer = request.referrer
+    if referrer and "login" in referrer:
+        if "vault_token" not in session:
+            return redirect("/")
 
-    if request.method == "POST":
-        inquiry = vakt.Inquiry(
-            action="Add",
-            resource="Ordine",
-            subject={"Role": session["role"]},
-        )
+        if request.method == "POST":
+            inquiry = vakt.Inquiry(
+                action="Add",
+                resource="Ordine",
+                subject={"Role": session["role"]},
+            )
 
-        if guard.is_allowed(inquiry):
+            if guard.is_allowed(inquiry):
+                content = request.form.get("content")
+                username = session["username"]
+                if content:
+                    new_note = Tabella(content=content, username=username)
+                    db.session.add(new_note)
+                    db.session.commit()
+                    
+                    flash("Pensiero aggiunto con successo!", "success")
+                    return redirect("/tabella")
+            else:
+                flash("Non hai i permessi per aggiungere un pensiero in questo momento.", "error")
+                return redirect("/tabella")
+    
+    else: 
+        if request.method == "POST":
             content = request.form.get("content")
             username = session["username"]
             if content:
                 new_note = Tabella(content=content, username=username)
                 db.session.add(new_note)
                 db.session.commit()
-                
-                flash("Nota aggiunta con successo!", "success")
+                    
+                flash("Pensiero aggiunto con successo!", "success")
                 return redirect("/tabella")
-        else:
-            flash("Non hai i permessi per aggiungere una nota in questo momento.", "error")
-            return redirect("/tabella")
 
 
     return render_template("add_note.html")
 
 @app.route("/edit-note/<int:id>", methods=["GET", "POST"])
 def edit_note(id):
-    if "vault_token" not in session:
-        return redirect("/")
+    referrer = request.referrer
+    if referrer and "login" in referrer:
+        if "vault_token" not in session:
+            return redirect("/")
 
-    note = Tabella.query.get_or_404(id)
+        note = Tabella.query.get_or_404(id)
 
-    if request.method == "POST":
-        inquiry = vakt.Inquiry(
-            action="Edit",
-            resource="Ordine",
-            subject={"Role": session["role"]},
-        )
+        if request.method == "POST":
+            inquiry = vakt.Inquiry(
+                action="Edit",
+                resource="Ordine",
+                subject={"Role": session["role"]},
+            )
 
-        if guard.is_allowed(inquiry):
+            if guard.is_allowed(inquiry):
+                new_content = request.form.get("content")
+                if new_content:
+                    note.content = new_content
+                    db.session.commit()
+                    
+                    
+                    flash("Nota modificata con successo!", "success")
+                    return redirect("/tabella")
+            else:
+                flash("Non hai i permessi per modificare questa nota in questo momento.", "error")
+                return redirect("/tabella")
+            
+    else: 
+        note = Tabella.query.get_or_404(id)
+        if request.method == "POST":
             new_content = request.form.get("content")
-            if new_content:
+            if new_content and session["role"] == "amministratore":
                 note.content = new_content
                 db.session.commit()
-                
-                
-                flash("Nota modificata con successo!", "success")
+                flash("Pensiero modificato con successo!", "success")
                 return redirect("/tabella")
-        else:
-            flash("Non hai i permessi per modificare questa nota in questo momento.", "error")
-            return redirect("/tabella")
+            
+            else:
+                flash("Non hai i permessi per modificare questo pensiero al momento.", "error")
+                return redirect("/tabella")
 
     return render_template("edit_note.html", note=note)
 
 @app.route("/delete-note/<int:id>", methods=["POST"])
 def delete_note(id):
-    if "vault_token" not in session:
-        return redirect("/")
+    referrer = request.referrer
+    if referrer and "login" in referrer:
+        if "vault_token" not in session:
+            return redirect("/")
 
-    note = Tabella.query.get_or_404(id)
+        note = Tabella.query.get_or_404(id)
 
-    inquiry = vakt.Inquiry(
-        action='Delete',
-        resource='Ordine',
-        subject={'Role': session['role']},
-    )
+        inquiry = vakt.Inquiry(
+            action='Delete',
+            resource='Ordine',
+            subject={'Role': session['role']},
+        )
 
-    if guard.is_allowed(inquiry):
-        db.session.delete(note)
-        db.session.commit()
+        if guard.is_allowed(inquiry):
+            db.session.delete(note)
+            db.session.commit()
 
-        flash("Nota eliminata con successo!", "success")
-        return redirect("/tabella")
-    else:
-        flash("Non hai i permessi per eliminare questa nota in questo momento.", "error")
-        return redirect("/tabella")
+            flash("Pensiero eliminato con successo!", "success")
+            return redirect("/tabella")
+        else:
+            flash("Non hai i permessi per eliminare questo pensiero al questo momento.", "error")
+            return redirect("/tabella")
+        
+    else: 
+        note = Tabella.query.get_or_404(id)
+
+        if session["role"] == "amministratore":
+            db.session.delete(note)
+            db.session.commit()
+
+            flash("Pensiero eliminato con successo!", "success")
+            return redirect("/tabella")
+        
+        else:
+            flash("Non hai i permessi per eliminare questo pensiero al momento.", "error")
+            return redirect("/tabella")
 
 @app.route("/logout")
 def logout():
+    end_session_endpoint = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/logout"
+
+    id_token = session['id_token']        
+    post_logout_redirect_uri = 'https://localhost:5173/'
+    params = { 'client_id': client_id, 'id_token_hint': id_token, 'post_logout_redirect_uri': post_logout_redirect_uri}
     session.clear()
-    return redirect("/")
+    response = requests.get(end_session_endpoint + '?' + urlencode(params))
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     # Configura il contesto SSL se necessario (modifica con i tuoi percorsi)
-    context=('/Users/francescoavallone/Desktop/UNIVERSITA/SystemSecurity/SysSec/HW345/cert1.crt', '/Users/francescoavallone/Desktop/UNIVERSITA/SystemSecurity/SysSec/HW345/key1.key')
+    context=('/Users/francescoavallone/Desktop/SysSec/HW345/certs/server.crt', '/Users/francescoavallone/Desktop/SysSec/HW345/certs/server.key')
     app.run(debug=True, host="localhost", port=5173, ssl_context=context)
+
+
